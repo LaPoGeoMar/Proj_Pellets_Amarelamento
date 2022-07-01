c75b0f4ff84ab5ac0fec4b78be7d7ac699244b2f
Juana Bozzetto <juanagbozzetto@gmail.com>
2022-07-01 03:51:48 -0300

    update classification routine

--- notebooks/02-classification.py
+++ notebooks/02-classification.py
@@ -1,26 +1,18 @@
-from pathlib import Path
-from glob import glob
+"""Classification of yellowness from the images."""
 
 import numpy as np
 import pandas as pd
 
-# classification of yellowness from the images
+df = pd.read_csv("RGB_values.csv", index_col=0)
 
-df = pd.read_csv('RGB_values.csv', index_col = 0)
-
-
-# create a list of our conditions
-conditions = [
-    (df['B'] >= 160) & (df['G']/df['B'] <= 1.5) & (df['G']/df['B'] > 0),
-    (df['B'] < 160) & (df['B'] >= 55),
-    (df['B'] < 55) & (df['B'] > 10),
-    (df['B'] <= 10) & (df['G'] >= 115),
-    (df['B'] <= 10) & (df['G'] < 115)
-    ]
-
-# create a list of the values we want to assign for each condition
-values = ['nulo', 'baixo', 'moderado', 'alto', 'muito alto']
+blue = {
+    "nulo":       (df["B"] >= 160),
+    "baixo":      (df["B"] <  160) & (df["B"] >  55),
+    "moderado":   (df["B"] <= 55)  & (df["B"] >  10),
+    "alto":       (df["B"] <= 10),
+    "muito alto": (df["B"] <= 10)
+}
 
 # create a new column and use np.select to assign values to it using our lists as arguments
-df['Grau de Amarelamento'] = np.select(conditions, values)
+df["Grau de Amarelamento"] = np.select(blue.values(), blue.keys())
 df.to_csv("pellets-RGB-classes.csv")
